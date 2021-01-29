ccc03878bbe86809b63e920211bfc487534d603a
missing009 <themissingarchi@gmail.com>
2021-01-29 15:55:44 +0200

    func 1/4

---
+++ tests/SeyHelloTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class SayHelloTest extends TestCase
+{
+    public function testPositive()
+    {
+        $this->assertEquals('Hello',sayHello());
+    }
+}
