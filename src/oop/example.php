6c6d953b11a07baea2619ef4ace42f647a68fbba
Станіслав Бабич <themissingarchi@gmail.com>
2021-02-18 11:35:39 +0200

    oop fix

--- src/oop/example.php
+++ src/oop/example.php
@@ -1,19 +1,21 @@
 <?php
 use src\oop\Calculator;
+use src\oop\Commands\DivCommand;
+use src\oop\Commands\ExpoCommand;
+use src\oop\Commands\MultiCommand;
 use src\oop\Commands\SubCommand;
 use src\oop\Commands\SumCommand;
 
 $calc = new Calculator();
 $calc->addCommand('+', new SumCommand());
 $calc->addCommand('-', new SubCommand());
+$calc->addCommand('*', new MultiCommand());
+$calc->addCommand('**', new ExpoCommand());
+$calc->addCommand('/', new DivCommand());
 
-// You can use any operation for computing
-// will output 2
-echo $calc->init(1)
-    ->compute('-', 1)
-    ->getResult();
 
-echo PHP_EOL;
+
+echo PHP_EOL. '<br/>';
 // Multiply operations
 // will output 10
 echo $calc->init(15)
@@ -21,16 +23,38 @@ echo $calc->init(15)
     ->compute('-', 10)
     ->getResult();
 
-echo PHP_EOL;
+echo PHP_EOL. '<br/>';
 // TODO implement replay method
-// should output 4
+// should output 2
 echo $calc->init(1)
     ->compute('+', 1)
-    ->replay()
-    ->replay()
     ->getResult();
 
-echo PHP_EOL;
+echo PHP_EOL. '<br/>';
+//(*)
+// should output 10
+
+echo $calc->init(5)
+    ->compute('*', 2)
+    ->getResult();
+
+echo PHP_EOL. '<br/>';
+
+//(/)
+// should output 4
+echo $calc->init(8)
+    ->compute('/', 2)
+    ->getResult();
+
+echo PHP_EOL. '<br/>';
+//(**)
+// should output 125
+
+echo $calc->init(5)
+    ->compute('**', 3)
+    ->getResult();
+
+echo PHP_EOL. '<br/>';
 // TODO implement undo method
 // should output 1
 echo $calc->init(1)
@@ -40,4 +64,4 @@ echo $calc->init(1)
     ->undo()
     ->getResult();
 
-echo PHP_EOL;
+echo PHP_EOL. '<br/>';
